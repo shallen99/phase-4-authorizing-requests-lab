fea86a718d63507a0164bed5fe636db64daf5ed6
+ <+>
2023-03-29 09:47:55 +0300

    done

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,20 +1,25 @@
 class MembersOnlyArticlesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
-  def index
-    articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    before_action :authorize
+    
+    def index
+      articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
+      render json: articles, each_serializer: ArticleListSerializer
+    end
+  
+    def show
+      article = Article.find(params[:id])
+      render json: article
+    end
+  
+    private
+  
+    def record_not_found
+      render json: { error: "Article not found" }, status: :not_found
+    end
+  
+    def authorize
+      return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+  
   end
-
-  def show
-    article = Article.find(params[:id])
-    render json: article
-  end
-
-  private
-
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
-  end
-
-end
